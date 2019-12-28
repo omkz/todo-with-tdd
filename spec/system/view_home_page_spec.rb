d4bcfb106c324f7c7e334443f19acd2d91f74075
Kurnia Muhamad <omenkzz@gmail.com>
2019-12-28 18:27:36 +0700

    Refactor smoke test

--- spec/system/view_home_page_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "Home page", type: :system do
-  it "responds successfully" do
-    visit root_path
-    
-    expect(page).to have_css("h1", text: "Todos")
-  end
-end
